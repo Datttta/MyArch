bd30654ffc09333d5a240a2b444e852bb0cbeb87
Diógenes <diogenesresplandes@gmail.com>
2025-05-19 15:02:51 -0300

    General update

---
+++ hyprland/switch_wallpaper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Array of wallpapers
+WALLPAPERS=(
+    "$HOME/Pictures/wallpaper/theMage.jpg"
+    "$HOME/Pictures/wallpaper/deepspacenine.jpg"
+    "$HOME/Pictures/wallpaper/sun.jpg"
+    "$HOME/Pictures/wallpaper/jellyjam.jpg"
+)
+
+# File to store index
+INDEX_FILE="$HOME/.config/hypr/.wallpaper_index"
+
+# Read index
+if [[ -f "$INDEX_FILE" ]]; then
+    index=$(cat "$INDEX_FILE")
+else
+    index=0
+fi
+# Next index
+next_index=$(( (index + 1) % ${#WALLPAPERS[@]} ))
+
+# Apply wallpaper
+hyprctl hyprpaper wallpaper "eDP-1,${WALLPAPERS[$next_index]}"
+
+# Generate pywal colors
+wal -i ${WALLPAPERS[$next_index]}
+
+# Save new index
+echo "$next_index" > "$INDEX_FILE"
+
--- zshrc/.zshrc
+++ zshrc/.zshrc
@@ -8,8 +8,18 @@ HISTSIZE=1000
 SAVEHIST=1000
 
 # Prompt (basic)
-PROMPT='%n@%m %1~ %# '
+#PROMPT='%n@%m %1~ %# '
+#PROMPT='%/ %# ' #show full path
 #PROMPT='%n@%m:%~> '
+
+# Show the current working directory above the prompt
+precmd() {
+  print -P "%F{green}%~%f"
+}
+
+# Minimal prompt on the second line where you type
+PROMPT="%F{blue}❯%f "
+
 
 # Enable suggestions and syntax highlighting (if installed)
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
