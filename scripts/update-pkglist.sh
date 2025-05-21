3f208ab294187bc940781537eaf0ea021e0e308f
Diógenes <diogenesresplandes@gmail.com>
2025-05-21 18:53:56 -0300

    General update

--- scripts/update-pkglist.sh
+++ scripts/update-pkglist.sh
@@ -3,9 +3,9 @@ echo "Updating package lists..."
 
 cd ~/dotfiles
 
-pacman -Qqen > pkglist.txt
-pacman -Qqem > aurlist.txt
-flatpak list --app --columns=application > flatpaklist.txt
+pacman -Qqen > packages/pkglist.txt
+pacman -Qqem > packages/aurlist.txt
+flatpak list --app --columns=application > packages/flatpaklist.txt
 
 git add .
 git commit -m "General update"
