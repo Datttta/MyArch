8d7303f4d6e11fcda63832e7b50042de7cafa355
Diógenes <diogenesresplandes@gmail.com>
2025-05-23 02:33:00 -0300

    hyprpaper.conf removed random wallpaper script added

---
+++ scripts/random-wallpaper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Pick a random wallpaper
+WALLPAPER=$(find ~/dotfiles/wallpaper -type f | shuf -n 1)
+
+# Generate a new Hyprpaper config
+cat > ~/.config/hypr/hyprpaper.conf <<EOF
+preload = $WALLPAPER
+wallpaper = eDP-1,$WALLPAPER
+EOF
+
+# Restart hyprpaper
+pkill hyprpaper
+hyprpaper &
+
