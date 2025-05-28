398163443699cb7537ca9309e199e03f1b27c6ec
Diógenes <diogenesresplandes@gmail.com>
2025-05-28 12:06:56 -0300

    New waybar restart in waypaper/.config/config.ini (new as well)

--- scripts/watch-waypaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIG="$HOME/.config/waypaper/config.ini"
-
-inotifywait -m -e close_write "$CONFIG" |
-while read -r directory events filename; do
-    bash ~/dotfiles/scripts/waybarRestart.sh
-done
-
--- scripts/waybarRestart.sh
+++ scripts/waybarRestart.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# Extract the current wallpaper path from Waypaper config
-WALLPAPER=$(grep '^wallpaper =' ~/.config/waypaper/config.ini | cut -d'=' -f2- | xargs)
-
-# Expand ~ to full path if needed
-WALLPAPER="${WALLPAPER/#\~/$HOME}"
-
-# Run pywal
-wal --cols16 lighten -i "$WALLPAPER"
-
-# Restart Waybar
-killall waybar
-waybar & 
+pkill -USR2 waybar
