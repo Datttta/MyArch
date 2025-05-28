b7c47e039b43f4944527169d774a55c4f538538d
Diógenes <diogenesresplandes@gmail.com>
2025-05-27 22:35:29 -0300

    perfect blank indent line color

--- nvim/.config/nvim/lua/plugins/blankline.lua
+++ nvim/.config/nvim/lua/plugins/blankline.lua
@@ -1,25 +1,41 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
   main = 'ibl',
-  opts = {
-    indent = {
-      char = '▏',
-    },
-    scope = {
-      show_start = false,
-      show_end = false,
-      show_exact_scope = false,
-    },
-    exclude = {
-      filetypes = {
-        'help',
-        'startify',
-        'dashboard',
-        'packer',
-        'neogitstatus',
-        'NvimTree',
-        'Trouble',
+  opts = function()
+    -- Get Nordic color palette
+    local palette = require('nordic.colors.nordic')
+
+    return {
+      indent = {
+        char = '▏',
+        highlight = 'IblIndentInactive', -- Custom highlight group
       },
-    },
-  },
+      scope = {
+        show_start = false,
+        show_end = false,
+        highlight = 'IblScopeActive', -- Custom scope highlight
+        show_exact_scope = false,
+      },
+      exclude = {
+        filetypes = {
+          'help', 'startify', 'dashboard', 'packer',
+          'neogitstatus', 'NvimTree', 'Trouble',
+        },
+      },
+    }
+  end,
+  config = function(_, opts)
+    -- Define highlight groups using Nordic colors
+    vim.api.nvim_set_hl(0, 'IblIndentInactive', {
+      fg = '#5c818e', -- #4C566A
+      nocombine = true,
+    })
+
+    vim.api.nvim_set_hl(0, 'IblScopeActive', {
+      fg = require('nordic.colors.nordic').blue2, -- #88C0D0
+      nocombine = true,
+    })
+
+    require('ibl').setup(opts)
+  end
 }
