0ea140d15f4df65f364b61216e2edd8407e4252b
Datttta <diogenesresplandes@gmail.com>
2025-12-11 13:45:44 -0300

    Fixed the issue of custom/expand not showing updates when it's available

--- waybar/.config/waybar/scripts/Check-update.sh
+++ waybar/.config/waybar/scripts/Check-update.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-# Get the number of updates from pacman (signal 8) and aur (signal 9) modules.
-# We are calling the same commands the modules use to get the raw numbers.
-
+sleep 2
 PACMAN_UPDATES=$(checkupdates | wc -l)
 AUR_UPDATES=$(yay -Qua | wc -l)
 FLATPAK_UPDATES=$(flatpak remote-ls --updates | wc -l)
 
 EXPAND_ICON=" <span size='150%'>󰃘</span> "
 ALERT_ICON=" <span size='150%'>󰃘</span> !"
+
 
 if [ "$PACMAN_UPDATES" -gt 0 ] || [ "$AUR_UPDATES" -gt 0 ] || [ "$FLATPAK_UPDATES" -gt 0 ]; then
     echo "${ALERT_ICON}"
