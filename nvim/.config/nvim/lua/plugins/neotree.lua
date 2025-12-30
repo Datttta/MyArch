ad47f8492ea22bd6cc584f038d011ab049c31ded
Datttta <diogenesresplandes@gmail.com>
2025-12-30 04:01:21 -0300

    No more searching recursively in neo tree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -291,7 +291,8 @@ return {
               ['<bs>'] = 'navigate_up',
               ['.'] = 'set_root',
               ['H'] = 'toggle_hidden',
-              ['/'] = 'fuzzy_finder',
+              ['/'] = 'noop',
+              ['z'] = 'fuzzy_finder',
               ['D'] = 'fuzzy_finder_directory',
               ['#'] = 'fuzzy_sorter', -- fuzzy sorting using the fzy algorithm
               -- ["D"] = "fuzzy_sorter_directory",
