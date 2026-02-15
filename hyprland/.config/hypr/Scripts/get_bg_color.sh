5dc4d0be6d0930882a075f012968016aa4ed2f2d
Datttta <diogenesresplandes@gmail.com>
2026-02-15 00:30:24 -0300

    waypaper fixed

---
+++ hyprland/.config/hypr/Scripts/get_bg_color.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Extract the current wallpaper path from Waypaper config
+WALLPAPER=$(grep '^wallpaper =' ~/.config/waypaper/config.ini | cut -d'=' -f2- | xargs)
+
+# Expand ~ to full path if needed
+WALLPAPER="${WALLPAPER/#\~/$HOME}"
+
+# copy image to a folder so hyprlock can use it
+mkdir -p ~/.cache/hyprlock
+cp "$WALLPAPER" ~/.cache/hyprlock/current_wallpaper
+
+# Run pywal
+wal --cols16 lighten -i "$WALLPAPER"
+
+#Reload kitty
+pgrep -x kitty | xargs -r kill -SIGUSR1
+
+killall swaync
+exec swaync
