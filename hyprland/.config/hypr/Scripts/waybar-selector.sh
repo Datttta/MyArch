0ec2302e02b2dddfa93283ffb697e24b38d1d07d
Datttta <diogenesresplandes@gmail.com>
2026-02-15 02:30:38 -0300

    waybar theme selector now shows the options in alphabetical order

--- hyprland/.config/hypr/Scripts/waybar-selector.sh
+++ hyprland/.config/hypr/Scripts/waybar-selector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="$HOME/.config/waybar/themes"
-THEME=$(ls "$DIR" | wofi --dmenu --normal-window "Waybar theme:")
+THEME=$(ls "$DIR" | wofi --dmenu --normal-window --sort-order=alphabetical "Waybar theme:")
 
 if [ -n "$THEME" ]; then
     ~/.config/waybar/scripts/waybar-switcher.sh "$THEME"
