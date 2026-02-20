0b88283d26b7d3f01be16b266a90be6d54020566
Datttta <diogenesresplandes@gmail.com>
2026-02-20 02:32:14 -0300

    Creating installation script

--- my_hyprland.sh
+++ my_hyprland.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 echo "Starting installation ((change this))(:"
+
+echo "Installing git..."
+sudo pacman -S git
 
 echo "Installing yay..."
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 rm -rf ~/yay
-
-echo "Installing git..."
-sudo pacman -S git
 
 echo "Installing flatpak"
 sudo pacman -S flatpak
@@ -19,6 +21,11 @@ sudo pacman -S stow
 echo "Stowing files!"
 cd
 git clone git@github.com:Datttta/MyArch.git
-cd dotfiles
+cd MyArch
 stow *
+
+# ===========================
+echo "Setting sddm"
+echo "/home/DROS/MyArch/sddm/themes/my-theme /usr/share/sddm/themes/my-theme none bind 0 0" | sudo tee -a /etc/fstab
+sudo mount -a
 
