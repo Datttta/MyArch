512486017692c8290bd0b95e250c95e28f0ac885
Datttta <diogenesresplandes@gmail.com>
2026-02-21 18:52:47 -0300

    btop added

--- my_hyprland.sh
+++ my_hyprland.sh
@@ -42,7 +42,7 @@ sudo setfacl -R -m u:sddm:rX,m::rX ~/MyArch/sl-sddm
 
 # ============================
 echo "Installing apps..."
-yay -S --noconfirm flatpak libreoffice kvantum copyq yazi fastfetch gamemode zsh wofi waybar swappy rofi pavucontrol kitty python-pywal16 wlogout swaync waypaper neovim
+yay -S --noconfirm flatpak libreoffice kvantum copyq yazi fastfetch gamemode zsh wofi waybar swappy rofi pavucontrol kitty python-pywal16 wlogout swaync waypaper neovim anki pear-desktop discord bluez bluez-utils lutris corectrl firefox steam gnome-clocks piper stremio osu timeshift timeshift-autosnap
 
 echo "Installing fonts..."
 yay -S --noconfirm ttf-dejavu ttf-fira-code ttf-jetbrains-mono-nerd ttf-liberation ttf-ubuntu-font-family woff2-font-awesome noto-fonts-cjk noto-fonts noto-fonts-extra noto-fonts-emoji otf-font-awesome ttf-radio-canada
