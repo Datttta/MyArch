0b517429a53fed6204201d459d6563984478ac39
Datttta <diogenesresplandes@gmail.com>
2026-04-01 16:17:21 -0300

    Changes on nvim keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -43,14 +43,20 @@ vim.keymap.set('n', '<Right>', ':vertical resize -2<CR>', opts)
 vim.keymap.set('n', '<Left>', ':vertical resize +2<CR>', opts)
 
 -- Buffers
---vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
---vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', 'L', ':bnext<CR>', opts)
+vim.keymap.set('n', 'H', ':bprevious<CR>', opts)
 vim.keymap.set('n', '<C-w>', ':Bdelete!<CR>', opts) -- close buffer
 vim.keymap.set('n', '<C-t>', '<cmd> enew <CR>', opts) -- new buffer
 
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+
 -- Window management
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
-vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+vim.keymap.set('n', '<leader>sv', '<C-w>v', opts) -- split window vertically
+vim.keymap.set('n', '<leader>sh', '<C-w>s', opts) -- split window horizontally
 vim.keymap.set('n', '<leader>=', '<C-w>=', opts) -- make split windows equal width & height
 vim.keymap.set('n', '<leader>x', ':close<CR>', opts) -- close current split window
 vim.keymap.set('n', '<leader>m', '<C-w>_', opts)
@@ -60,12 +66,6 @@ vim.keymap.set('n', '<leader>k', ':wincmd k<CR>', opts)
 vim.keymap.set('n', '<leader>j', ':wincmd j<CR>', opts)
 vim.keymap.set('n', '<leader>h', ':wincmd h<CR>', opts)
 vim.keymap.set('n', '<leader>l', ':wincmd l<CR>', opts)
-
--- Tabs
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
-vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
 
 -- Toggle line wrapping
 vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
