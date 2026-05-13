d466984f242d70fed4c75182431801e6892e0070
Datttta <diogenesresplandes@gmail.com>
2026-05-13 13:32:47 -0300

    ricing 17 added

---
+++ storage/scripts/Ricing-showcase/ricing-17.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Function to wait for a window of a specific class to appear
+wait_for_window() {
+    local target_class=$1
+    # Max attempts to prevent infinite loops (approx 5 seconds)
+    local attempts=50 
+    while [ $attempts -gt 0 ]; do
+        if hyprctl clients -j | jq -e ".[] | select(.class == \"$target_class\")" > /dev/null; then
+            return 0
+        fi
+        sleep 0.1
+        ((attempts--))
+    done
+    return 1
+}
+
+check_class() {
+    local target_class=$1
+    while hyprctl clients -j | jq -e --arg class "$target_class" '.[] | select(.class == $class)' > /dev/null; do
+        echo "class already exists: $target_class" >&2
+        echo "changing name..." >&2
+        #check if there is a number at the end of the variable
+        if [[ $target_class =~ ([0-9]+)$ ]]; then 
+            number="${BASH_REMATCH[1]}"
+            prefix="${target_class%$number}"
+
+            target_class="${prefix}$((number + 1))"
+            echo "currect class name: $target_class" >&2
+        else
+            target_class="${target_class}1"
+        fi
+    done
+
+    echo "final class name: $target_class" >&2
+    echo "$target_class"
+}
+
+hyprctl dispatch movecursor 0 0
+
+# Open Yazi
+app=$(check_class "cava_term")
+kitty --class "$app" cava &
+wait_for_window "$app"
+
+# Open Nvim
+app=$(check_class "kitty_term")
+kitty --class $app &
+wait_for_window $app
+
+# bottom-left
+hyprctl dispatch movecursor 450 700
+app=$(check_class "hpyrland-colors_term")
+kitty --class $app nvim ~/.cache/wal/colors-hyprland.conf &
+wait_for_window $app
+
+# Extra bottom-left tenki
+hyprctl dispatch movecursor 750 700
+app=$(check_class "tenki_term")
+kitty --class $app tenki &
+wait_for_window $app
+
+# Bottom-right
+hyprctl dispatch movefocus r
+hyprctl dispatch movecursor 1450 700
+app=$(check_class "btop_term")
+kitty --class $app btop &
+wait_for_window $app
+
+# Extra top-right
+hyprctl dispatch movefocus u
+app=$(check_class "cmatrix_term")
+kitty --class $app cmatrix -b &
+wait_for_window $app
+
+# Open wofi
+wofi --show drun --normal-window &
+hyprctl dispatch focuswindow class:^wofi$
+wait_for_window "wofi"
+
+# Open swaync
+swaync-client -t &
+
