7bf16431a8883fa0d640b596ac7c180b423a8ff9
Datttta <diogenesresplandes@gmail.com>
2026-05-27 19:40:10 -0300

    custom background color for arc.jpg created

--- hypr/.config/hypr/Scripts/get_bg_color.sh
+++ hypr/.config/hypr/Scripts/get_bg_color.sh
@@ -13,11 +13,38 @@ cp "$WALLPAPER" ~/.cache/hyprlock/current_wallpaper
 # Run pywal
 wal --cols16 lighten -i "$WALLPAPER"
 
-#Reload kitty
-pgrep -x kitty | xargs -r kill -SIGUSR1
+# change order of the colors based on wallpaper
+echo "wallpaper: $(basename "$WALLPAPER")"
+
+if [[ $(basename "$WALLPAPER") == "arc.jpg" ]]; then
+
+    echo "changing background" > /tmp/get_bg_color.log
+    
+    current_color=$(grep "background" ~/.cache/wal/colors-kitty.conf | awk '{print $2}')
+    new_color=$(convert xc:"$current_color" -modulate 45 -format "%[hex:u]" info:)
+
+    # Apply the new color
+    sed -i "s/^background.*/background #$new_color/" ~/.cache/wal/colors-kitty.conf
+
+    #file="$HOME/.cache/wal/rgba-colors.lua"
+
+    #color1=$(grep "color1 =" "$file" | cut -d'"' -f2)
+    #color14=$(grep "color14 =" "$file" | cut -d'"' -f2)
+
+    #sed -i "s|color1 = \".*\"|color1 = \"$color14\"|" "$file"
+    #sed -i "s|color14 = \".*\"|color14 = \"$color1\"|" "$file"
+
+    #Reload kitty
+    for sock in /tmp/mykitty-*; do
+        kitty @ --to "unix:$sock" set-colors -a ~/.cache/wal/colors-kitty.conf
+    done
+
+fi
+
 
 #Reolad hyprland
 hyprctl reload
 
 killall swaync
 exec swaync
+
