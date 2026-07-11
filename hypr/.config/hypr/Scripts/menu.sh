b2ae6380ecb4882d3e3f14dd9b3301aac45b64dd
Datttta <diogenesresplandes@gmail.com>
2026-07-11 03:59:36 -0300

    waybar switch improved

--- hypr/.config/hypr/Scripts/menu.sh
+++ hypr/.config/hypr/Scripts/menu.sh
@@ -169,16 +169,8 @@ case "$option" in
             BASE="$HOME/.config/waybar"
             THEME_DIR="$BASE/themes/$THEME"
 
-            # Remove old links
-            rm -f "$BASE/config.jsonc"
-            rm -f "$BASE/style.css"
-
-            # Create new symlinks
-            ln -s "$THEME_DIR/config.jsonc" "$BASE/config.jsonc"
-            ln -s "$THEME_DIR/style.css" "$BASE/style.css"
-
             killall waybar
-            waybar & disown
+            waybar -c "$THEME_DIR/config.jsonc" -s "$THEME_DIR/style.css" & disown
         fi ;;
 
     "Configs") 
