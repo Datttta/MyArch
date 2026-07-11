41cae8e55fdace066600051585a8f42b77d11914
Datttta <diogenesresplandes@gmail.com>
2026-07-11 01:07:13 -0300

    improved waybar_hub.sh

--- waybar/.config/waybar/scripts/waybar_hub.sh
+++ waybar/.config/waybar/scripts/waybar_hub.sh
@@ -1,76 +1,67 @@
 #!/bin/bash
 echo "get $1" >&2
 
+wait_for_network() {
+    while true; do
+        /usr/bin/checkupdates &>/dev/null
+        exit_code=$?
+        
+        # 0: updates available | 2: no updates
+        if [[ $exit_code -eq 0 || $exit_code -eq 2 ]]; then
+            echo "connection working: $exit_code" >&2
+            break
+        fi
+        echo "no connection: $exit_code" >&2
+        sleep 1
+    done
+}
+
+yay_updates() {
+    echo $(( $(/usr/bin/checkupdates | wc -l) + $(/usr/bin/yay -Qua | wc -l) ))
+}
+
+flatpak_updates() {
+    echo $(/usr/bin/flatpak remote-ls --updates | wc -l)
+}
+
 case "$1" in
     "yay")
         # run yay
         hyprctl dispatch "hl.dsp.exec_cmd(\"kitty -e bash -lc 'yay; kill -SIGRTMIN+2 $(pidof waybar)'\")"
         ;;
+
     "flatpak")
         # run flatpak update
         hyprctl dispatch "hl.dsp.exec_cmd(\"kitty -e bash -lc 'flatpak update; kill -SIGRTMIN+2 $(pidof waybar)'\")"
         ;;
+   
     "network")
         # open network app
         hyprctl dispatch "hl.dsp.exec_cmd('kitty -e nmtui')"
         ;;
+   
     "get_yay_numbers")
-        ### Change this to a continous loop, only stops when network is back
-        for i in {1..10}; do
-            /usr/bin/checkupdates &>/dev/null
-            exit_code=$?
-            
-            # If it returns 0 or 2, it successfully reached the Arch mirrors.
-            if [[ $exit_code -eq 0 || $exit_code -eq 2 ]]; then
-                break
-            fi
-            sleep 1
-        done
-
-        YAY_UPDATES=$(( $(/usr/bin/checkupdates | wc -l) + $(/usr/bin/yay -Qua | wc -l) ))
+        wait_for_network
+        YAY_UPDATES=$(yay_updates)
         echo $YAY_UPDATES
         
         echo "yay updates from get_yay_numbers: $YAY_UPDATES" >&2
         ;;
 
     "get_flatpak_numbers")
-        ### Change this to a continous loop, only stops when network is back
-        for i in {1..10}; do
-            /usr/bin/checkupdates &>/dev/null
-            exit_code=$?
-            
-            # If it returns 0 or 2, it successfully reached the Arch mirrors.
-            if [[ $exit_code -eq 0 || $exit_code -eq 2 ]]; then
-                break
-            fi
-            sleep 1
-        done
-        
-        FLATPAK_UPDATES=$(/usr/bin/flatpak remote-ls --updates | wc -l)
+        wait_for_network
+        FLATPAK_UPDATES=$(flatpak_updates)
         echo $FLATPAK_UPDATES
 
         echo "flatpak updaets from get_flatpak_numbers: $FLATPAK_UPDATES" >&2
         ;;
 
-# add case get_flatpak_numbers
-
     "update")
         # check updates
-        # Wait up to 10 seconds for both internet AND DNS to be fully ready
-        ### Change this to a continouss loop, only stops when network is back
-        for i in {1..10}; do
-            /usr/bin/checkupdates &>/dev/null
-            exit_code=$?
-            
-            # If it returns 0 or 2, it successfully reached the Arch mirrors.
-            if [[ $exit_code -eq 0 || $exit_code -eq 2 ]]; then
-                break
-            fi
-            sleep 1
-        done
+        wait_for_network
 
-        YAY_UPDATES=$(( $(/usr/bin/checkupdates | wc -l) + $(/usr/bin/yay -Qua | wc -l) ))
-        FLATPAK_UPDATES=$(/usr/bin/flatpak remote-ls --updates | wc -l)
+        YAY_UPDATES=(yay_updates)
+        FLATPAK_UPDATES=(flatpak_updates)
 
         EXPAND_ICON=" <span size='150%'>󰃘</span> "
         ALERT_ICON=" <span size='150%'>󰃘</span> !"
